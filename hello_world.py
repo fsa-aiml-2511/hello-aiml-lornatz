dac3241fd139006e07bdc48dccec6f49f3b906ef
lornatz <lornatz1@yahoo.com>
2025-12-18 18:59:14 -0700

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,8 @@
 # Hello AI/ML Bootcamp!
 # Edit the variables below to introduce yourself
 
-name = "Your Name Here"
-fun_fact = "One fun fact about me"
+name = "lorna"
+fun_fact = "dancing"
 
 # Don't change anything below this line
 print(f"Hello! I'm {name}.")
